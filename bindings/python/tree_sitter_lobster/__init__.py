8fca5d57d15482c1cce0f82c98203eb71ffcadaa
inferrna <inferrna@gmail.com>
2024-07-24 18:21:28 +0400

    Add generated

---
+++ bindings/python/tree_sitter_lobster/__init__.py
@@ -0,0 +1,5 @@
+"Lobster grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/swift/TreeSitterLobster/lobster.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LOBSTER_H_
+#define TREE_SITTER_LOBSTER_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_lobster(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_LOBSTER_H_
